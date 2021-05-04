531158264ece409edcd71b0f8d7ad934c38bd3fa
mhomola <mhomola@student.tudelft.nl>
2021-05-04 11:56:47 +0200

    Improved volume estimation

--- fuel_required.py
+++ fuel_required.py
@@ -1,10 +1,12 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from fuel_constants import *
 
-def fuel_volume_calc(d_H2, d_k, Ed_H2, Ed_k, tot_vol_k, f_ratio):
+def fuel_volume_calc(d_LH2, d_GH2, d_k, Ed_H2, Ed_k, tot_vol_k, e_ratio, y = 0.95):
     """
 
-    :param d_H2: density of hydrogen [kg/m^3]
+    :param d_LH2: density of liquid hydrogen [kg/m^3]
+    :param d_LH2: density of gaseous hydrogen [kg/m^3]
     :param d_k: density of kerosene [kg/m^3]
     :param Ed_H2: energy density of hydrogen [kWh/kg]
     :param Ed_k: energy density of kerosene [kWh/kg]
@@ -14,20 +16,43 @@ def fuel_volume_calc(d_H2, d_k, Ed_H2, Ed_k, tot_vol_k, f_ratio):
     """
 
     E_tot = Ed_k * d_k / 1000 * tot_vol_k
-    E_k = (1 - f_ratio) * E_tot
-    E_H2 = f_ratio * E_tot
+
+    r = 1/(1+e_ratio)
+
+    E_k = E_tot*r
+    E_H2 = e_ratio * E_k
 
     V_k = E_k / Ed_k / d_k * 1000
-    V_H2 = E_H2 / Ed_H2 / d_H2 * 1000
+
+    V_LH2 = (E_H2/Ed_H2)*(1/(d_LH2+d_GH2*(1-y)/y))
+    V_H2 = V_LH2*(1+(1-y)/y)*1000
 
     return V_k, V_H2
 
 # Compute volumes
 
-Fuel_ratio = 0.2 # [-]
-fuel_capacity_a320neo = 20000 # [l]
-V_k, V_H2 = fuel_volume_calc(d_H2=LH2_d, d_k=k_d, Ed_H2=H2_ed, Ed_k=k_ed, tot_vol_k=fuel_capacity_a320neo,
-                             f_ratio=Fuel_ratio)
+E_ratio = 0.15 # [-]
+fuel_capacity_a320neo = 26730 # [l]
+V_k, V_H2 = fuel_volume_calc(d_LH2=LH2_d, d_GH2= GH2_d, d_k=k_d, Ed_H2=H2_ed, Ed_k=k_ed, tot_vol_k=fuel_capacity_a320neo,
+                             e_ratio=E_ratio)
 
-print('Volume of LH2 is ', V_H2, ' l')
+print('Volume of H2 is ', V_H2, ' l')
 print('Volume of kerosene is ', V_k, ' l')
+
+e_ratios = np.arange(0.1,0.3,0.001)
+V_k, V_H2 = fuel_volume_calc(d_LH2=LH2_d, d_GH2= GH2_d, d_k=k_d, Ed_H2=H2_ed, Ed_k=k_ed, tot_vol_k=fuel_capacity_a320neo,
+                             e_ratio=e_ratios)
+V_tot = V_k+V_H2
+
+f1 = plt.figure()
+ax1 = f1.add_subplot(311)
+ax1.plot(e_ratios,V_H2)
+ax1.set_ylabel('H2 Volume [l]')
+ax2 = f1.add_subplot(312)
+ax2.plot(e_ratios, V_k)
+ax2.set_ylabel('Kerosene Volume [l]')
+ax3 = f1.add_subplot(313)
+ax3.plot(e_ratios, V_tot)
+ax3.set_xlabel('Fuel Energy Ratio [-]')
+ax3.set_ylabel('Total Fuel Volume [l]')
+plt.show()
