9a396d5f8defc33adf72af90466ca4a5590d1638
Micha Nikitin <m.nikitin@student.tudelft.nl>
2021-05-06 17:29:09 +0200

    Aerodynamics template

--- fuel_constants.py
+++ fuel_constants.py
@@ -1,4 +1,4 @@
-def gas_law(p,T):
+def gas_law(p, T):
     return p*M_H2/(R*T)
 
 H2_ed = 33.5                                      # Energy density of hydrogen                         [kWh/kg]
@@ -11,7 +11,7 @@ LH2_d = 65.0                                      # Mass density of liquid hydro
 # H2_d = 38.0                                     # Mass density of gaseous hydrogen stored at 70 MPa  [kg/m^3]
 M_H2 = 2.016*10**(-3)                             # Molar mass of H2                                   [kg/mole]
 R = 8.314                                         # Gas constant                                       [J/(mol*K)]
-GH2_d = gas_law(p_tank,T_tank)
+GH2_d = gas_law(p_tank, T_tank)
 k_d = 810.0                                       # Mass density of kerosene                           [kg/m^3]
 y = 0.95                                          # LH2 volume ratio (from Winnefeld et al.)           [-]
 
