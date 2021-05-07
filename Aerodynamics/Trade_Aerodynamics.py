a29cf5f2f77e3aecee9456eaa8319843e0cbae83
Micha Nikitin <m.nikitin@student.tudelft.nl>
2021-05-07 16:42:23 +0200

    Adding small stuff

--- Aerodynamics/Trade_Aerodynamics.py
+++ Aerodynamics/Trade_Aerodynamics.py
@@ -2,6 +2,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import interpolate
 import math as m
+
+
+def fus_wet_surface(l_cockpit, l_cabin, l_tail, df):
+    """
+    :param l_cockpit: length of the cockpit in [m]
+    :param l_cabin: length of the cabin in [m]
+    :param l_tail: length of the tail in [m]
+    :param df: diameter length in [m]
+    :return: Wet surface of the fuselage in [m^2]
+    """
+
+    S_w_fus = m.pi * df/4 * (1/3/ l_cockpit **2 * ((4*l_cockpit**2+df**2/4)-df**3/8) - df + 4 * l_cabin + 2 * m.sqrt(l_tail**2+ df**2/4))
+
+    return S_w_fus
+
+
 """ Concept 1: Cargo Hold"""
 # Aerodynamic impact is 0
 
@@ -11,6 +27,16 @@ import math as m
 """ Concept 4: Flat Bottom """
 
 """ Concept 5: Wing Pods """
+pods_number = 2
+pods_cdo = 0.025
+pods_interf = 1.3
+
+pods_L1 = 0.5
+pods_main_length = 5
+pods_L3 = 0.5
+
+S_w_pods = fus_wet_surface(pods_L1, pods_main_length, pods_L3)
+pods_contrib_cdo = pods_cdo*pods_interf*S_w_pods #todo: divide by wet surface of the plane and by Cdo of A320
 
 """ Concept 6: Beluga """
 
@@ -36,13 +62,13 @@ def graph_method(lf, df, new_df):
     curr_ratio = lf/df
 
     for i in ratios:
-        if abs(curr_ratio-i)<0.01:
+        if abs(curr_ratio-i) < 0.01:
             curr_cd = cd_graph(curr_ratio)
 
     new_ratio = lf/new_df
 
     for i in ratios:
-        if abs(new_ratio-i)<0.01:
+        if abs(new_ratio-i) < 0.01:
             new_cd = cd_graph(new_ratio)
 
     plt.plot(ratios, cd_graph(ratios), label = "Front Cd")
@@ -58,7 +84,7 @@ def graph_method(lf, df, new_df):
     #Compute the drag variation
 
     cd_var = (new_cd-curr_cd)/curr_cd*100 # [%]
-    return cd_var,curr_cd,new_cd
+    return cd_var, curr_cd, new_cd
 
 #######INSERT HERE DESIRED PARAMETERS
 
@@ -66,26 +92,14 @@ lf = 37.57 #fuselage length in [m]
 df = 4.14  #fuselage maximum diameter in [m]
 new_df = 5 #modified maximum diameter in [m]
 
-cd_var_graph,curr_cd_graph,new_cd_graph = graph_method(lf, df, new_df) #cd variation calculated with the garphical drag
-
-def fus_wet_surface(l_cockpit,l_cabin,l_tail,df):
-    """
-    :param l_cockpit: length of the cockpit in [m]
-    :param l_cabin: length of the cabin in [m]
-    :param l_tail: length of the tail in [m]
-    :param df: diameter length in [m]
-    :return: Wet surface of the fuselage in [m^2]
-    """
-
-    S_w_fus = m.pi * df/4 * (1/3/ l_cockpit **2 * ((4*l_cockpit**2+df**2/4)-df**3/8)- df + 4* l_cabin + 2 * m.sqrt(l_tail**2+ df**2/4))
-
-    return S_w_fus
+cd_var_graph, curr_cd_graph, new_cd_graph = graph_method(lf, df, new_df) #cd variation calculated with the garphical drag
 
 l_cockpit = 5.04 #[m]
 l_cabin = 29.53 - l_cockpit # [m]
 l_tail = lf - 29.53 #[m]
 
-S_w_fus = fus_wet_surface(l_cockpit,l_cabin,l_tail,df) #wetted surface area of the standard configuration
+S_w_fus = fus_wet_surface(l_cockpit, l_cabin, l_tail, df) #wetted surface area of the standard configuration
+
 
 def mgc(ct,cr,sweep,df):
     """
