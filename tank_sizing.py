cf96ed95c37f1a990a3f047999dcb8228423256d
mgiobbio <mgiobbio@student.tudelft.nl>
2021-05-11 09:45:33 +0200

    H gas tank given tot volume function

--- tank_sizing.py
+++ tank_sizing.py
@@ -10,10 +10,11 @@ import matplotlib.pyplot as plt
 
 
 def hydrogen_tank_mass(State):
-
+    #This function takes the state of the hydrogen: 'liquid' or 'gas'; and computes the mass of the tank's mass
+    #for different fuel ratios (ratio between kerosene and H2) and different gravimetric efficiencies
     if State == 'liquid':
 
-        Grav_eff = np.arange(0.25,0.38,0.05)                             #Gravimetric efficiency if we choose liquid H2
+        Grav_eff = np.arange(0.25,0.38,0.05)                            #Gravimetric efficiency if we choose liquid H2
         H2_mass = fuel_mass_calc(State=State,                           #Array of H2 masses for all
                                  d_k=k_d, d_LH2=LH2_d, d_GH2=GH2_d)[1]  #feasible volumes of H2
         System_mass = np.ones((len(H2_mass),len(Grav_eff)))
@@ -68,7 +69,10 @@ def plotting_sys_mass(State):
 
 plotting_sys_mass(State='liquid')
 
-
+def liquid_H_tanks(H2_vol):
+    #input: volume available to store H2
+    #output: tank mass, system mass and H2 mass
+    Grav_eff = np.arange(0.25, 0.38, 0.05)                              # Gravimetric efficiency if we choose liquid H2
 
 
 
@@ -135,3 +139,64 @@ def gas_H_tanks(H2_vol):
     return None
 
 gas_H_tanks(297)
+gas_H_tanks(41743.13)
+
+# Visualize stuff for the gas tanks given the max available volume
+
+def gas_H_tanks_VOL(tot_Vol):
+
+    mass = []
+    volume = []
+    massH = []
+    volumeH = []
+    p_ran = np.arange(350,705,5)
+
+    for p in p_ran:
+       H_vol_frac = 92 - 11/350 * p
+       H_vol = H_vol_frac/100 * tot_Vol
+       d = -3.552714e-15 + 0.07845238*p - 0.00004285714*p**2 + 1.190476e-8*p**3
+       H_mass = H_vol*0.001*d
+
+       H_mass_frac = 7 - 1/350 * p
+       tank_mass = (H_mass*100)/H_mass_frac
+
+       mass.append(tank_mass)
+       volume.append(tot_Vol)
+       massH.append(H_mass)
+       volumeH.append(H_vol)
+
+    plt.figure()
+
+    plt.subplot(411)
+    plt.plot(p_ran, mass, color='tab:blue', marker='x',label='total mass')
+    plt.xlabel("Pressure [bar]")
+    plt.ylabel("Tank mass [kg]")
+    plt.legend()
+    plt.grid()
+
+    plt.subplot(412)
+    plt.plot(p_ran, massH, color='tab:red', marker='o',label='H2 mass')
+    plt.xlabel("Pressure [bar]")
+    plt.ylabel("Hydrogen mass [kg]")
+    plt.legend()
+    plt.grid()
+
+    plt.subplot(413)
+    plt.plot(p_ran, volume, color='tab:orange', marker='x',label='total volume')
+    plt.xlabel("Pressure [bar]")
+    plt.ylabel("Tank volume [l]")
+    plt.legend()
+    plt.grid()
+
+    plt.subplot(414)
+    plt.plot(p_ran, volumeH, color='tab:green', marker='o',label='H2 volume')
+    plt.xlabel("Pressure [bar]")
+    plt.ylabel("Hydrogen volume [l]")
+    plt.legend()
+    plt.grid()
+
+    plt.show()
+
+    return None 
+
+#gas_H_tanks_VOL(500)  
