33d8ef3ff600ec1a3f0345027d3205025581f40d
Micha Nikitin <m.nikitin@student.tudelft.nl>
2021-05-14 12:11:51 +0200

    Rearranging

--- Subsystem_design/Tank Design/Geometrical_Design.py
+++
@@ -1,3 +0,0 @@
-"""
-This files contains the classes and fucntions for determining the geometrical design of the tank (cross-section,length).
-"""
--- Subsystem_design/Tank Design/Mechanical_Design.py
+++ Subsystem_design/Tank Design/Mechanical_Design.py
@@ -1,13 +1,22 @@
 """
 This files contains the classes and functions for determining the mechanical design of the tank (thickness of the wall).
 """
-import Materials
+import numpy as np
+import matplotlib.pyplot as plt
 
 #Hydrogen tank storage data
+p_out = 0.19028  # [bar] at 12100 [m] of altitude
+p_tank = 3         # [bar]
 
-p_tank = 3  #[bar] temperature inside the tank
-p_out = 0.19 #[bar] temperature outside the tank for cruising conditions @12[km]
+t_out = 216.65   #[k]
+t_tank = 20      #[k]
 
-T_tank = 20 #[k]
-T_out = 216.65 #[k]
+
+dp = np.abs(p_tank-p_out)
+dt = np.abs(t_tank-t_tank)
+
+def wall_thickness():
+    s_a =
+
+
 
