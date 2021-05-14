67d4847c43fea06a1805c15239cc883c195c7083
nicusoara1 <62592687+nicusoara1@users.noreply.github.com>
2021-05-14 12:08:02 +0200

    .

--- Subsystem_design/Tank Design/Mechanical_Design.py
+++ Subsystem_design/Tank Design/Mechanical_Design.py
@@ -1,3 +1,13 @@
 """
 This files contains the classes and functions for determining the mechanical design of the tank (thickness of the wall).
 """
+import Materials
+
+#Hydrogen tank storage data
+
+p_tank = 3  #[bar] temperature inside the tank
+p_out = 0.19 #[bar] temperature outside the tank for cruising conditions @12[km]
+
+T_tank = 20 #[k]
+T_out = 216.65 #[k]
+
