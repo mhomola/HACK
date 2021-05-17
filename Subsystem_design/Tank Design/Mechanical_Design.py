321dd7c99beb92ccef9a3fbc4ec0f00c5e2e6c39
Micha Nikitin <m.nikitin@student.tudelft.nl>
2021-05-17 15:03:01 +0200

    Materials and Mechanical design cahnges

--- Subsystem_design/Tank Design/Mechanical_Design.py
+++ Subsystem_design/Tank Design/Mechanical_Design.py
@@ -5,19 +5,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Hydrogen tank storage data
-p_out = 0.19028  # [bar] at 12100 [m] of altitude
-p_tank = 3         # [bar]
+p_outside = 0.19028  # [bar] at 12100 [m] of altitude
+p_tank = 3           # [bar]
+t_outside = 216.65   # [k]
+t_tank = 20          # [k]
+d_0 = 4              # [m] outside diameter -- dummy
+e_w = 0.8            # weld efficiency from Barron, 1985
+s_a = 200            # [MPa] allowable stress dummy value for now!!
 
-t_out = 216.65   #[k]
-t_tank = 20      #[k]
+dp = np.abs(p_tank-p_outside)
+dt = np.abs(t_tank-t_outside)
 
 
-dp = np.abs(p_tank-p_out)
-dt = np.abs(t_tank-t_tank)
+K = 1/6 * (2 + )
 
+def thickness(dp, d_0, s_a, e_w, K):
+  t_wall = dp * d_0 / (2*s_a*e_w + 0.8*dp)
+  t_caps = dp * d_0 * K / (2*s_a*e_w + 2*dp*(K-0.1))
+  return t_wall, t_caps
 
-def wall_thickness():
-    s_a =
+#choose tank dimensions depending on constraints
 
 
 
