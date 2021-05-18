711c51f0435f91319b727fd25d496775d8d3a5ed
Micha Nikitin <m.nikitin@student.tudelft.nl>
2021-05-18 08:52:25 +0200

    Materials and Mechanical design cahnges

--- Subsystem_design/Tank Design/Materials.py
+++ Subsystem_design/Tank Design/Materials.py
@@ -13,7 +13,8 @@ class Material():
 Al_6061_T6 = Material(E=68.9, yield_strength=324, ult_strength=414, density=2700, conductivity=167)
 Al_2219_T87 = Material(E=73.1, yield_strength=393, ult_strength=476, density=2840, conductivity=121)
 Al_2090_T86 = Material(E=76, yield_strength=520, ult_strength=550, density=2590, conductivity=88)
-Al_8090_T8151 = Material(E=77, yield_strength=450, ult_strength=370, density=2540, conductivity=95.3)
+Al_8090_T8151 = Material(E=88, yield_strength=463, ult_strength=370, density=2540, conductivity=95.3)
+
 
 class Insulation_Material():
     def __init__(self, conductivity, diffusivity, density):
