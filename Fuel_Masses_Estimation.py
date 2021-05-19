e0247bf668c9bc8ea851279895732de0cb87e619
mhomola <mhomola@student.tudelft.nl>
2021-05-19 10:50:15 +0200

    mission profile

--- Fuel_Masses_Estimation.py
+++ Fuel_Masses_Estimation.py
@@ -33,7 +33,7 @@ t_climb = 25*60
 t_descend = 25*60
 
 V_ker = fc.fuel_capacity_a320neo
-m_ker_all = fc.fuel_capacity_a320neo*fc.k_d/1000
+m_ker_all = 27000#fc.fuel_capacity_a320neo*fc.k_d/1000
 
 E_tot = m_ker_all*LHV_k
 E_h2 = (1/3)*E_tot
@@ -71,24 +71,12 @@ h2m_taxi_i = 2*ff_taxi_i*t_taxi_in
 
 h2_m_rem = m_h2-(h2m_idle+h2m_taxi_o+h2m_taxi_i)
 
-r = h2_m_rem/(h2_m_rem+m_ker)
-
-#masses of kerosene
-mk_init = m_ker
-ff_climb_k = 2*fuel_flow(T_frac_Climb,sfc_ker*(1-r))
-ff_cruise_k = 2*fuel_flow(T_frac_Cruise,sfc_ker*(1-r))
-ff_desc_k = 2*fuel_flow(T_frac_Descent,sfc_ker*(1-r))
+r = h2_m_rem*LHV_H2/(h2_m_rem*LHV_H2+m_ker*LHV_k)
 
 
-
-mk_climb = mk_init - ff_climb_k*t_climb
-mk_cruise = mk_init - ff_climb_k*t_climb - ff_desc_k*t_descend #mk_climb - 2*ff_cruise_k*t_cruise
-mk_desc = mk_cruise - ff_desc_k*t_descend
-# ---
-
-ff_climb_h2 = r*ff_climb_k #fuel_flow(T_frac_Climb,sfc_h2*r)
-ff_cruise_h2 = r*ff_cruise_k #fuel_flow(T_frac_Cruise,sfc_h2*r)
-ff_desc_h2 = r*ff_desc_k #fuel_flow(T_frac_Descent,sfc_h2*r)
+ff_climb_h2 = fuel_flow(T_frac_Climb,sfc_h2*r)
+ff_cruise_h2 = fuel_flow(T_frac_Cruise,sfc_h2*r)
+ff_desc_h2 = fuel_flow(T_frac_Descent,sfc_h2*r)
 #ff_climb = fuel_flow(T_frac_Climb,s)
 
 h2m_climb = 2*ff_climb_h2*t_climb
@@ -96,7 +84,18 @@ h2m_desc = 2*ff_desc_h2*t_descend
 
 h2_m_rem2 = h2_m_rem - (h2m_climb+h2m_desc)
 
-t_cruise = h2_m_rem2/ff_cruise_h2
+t_cruise = h2_m_rem2/(2*ff_cruise_h2)
+
+#masses of kerosene
+mk_init = m_ker
+ff_climb_k = 2*fuel_flow(T_frac_Climb,sfc_ker*(1-r))
+ff_cruise_k = 2*fuel_flow(T_frac_Cruise,sfc_ker*(1-r))
+ff_desc_k = 2*fuel_flow(T_frac_Descent,sfc_ker*(1-r))
+
+mk_climb = mk_init - ff_climb_k*t_climb
+mk_cruise = mk_climb - ff_cruise_k*t_cruise
+mk_desc = mk_cruise - ff_desc_k*t_descend
+# ---
 
 #representative times in flight
 t0 = 0
@@ -115,7 +114,7 @@ mh2_cruise = mh2_climb - h2_m_rem2
 mh2_desc = mh2_cruise - h2m_desc
 mh2_taxi_i = mh2_desc - h2m_taxi_i
 
-print(1-r)
+print(r)
 
 t = [t0,t1,t2,t3,t4,t5,t6]
 fuel_h2 = [m_h2,mh2_idle, mh2_taxi_o, mh2_climb, mh2_cruise, mh2_desc, mh2_taxi_i]
